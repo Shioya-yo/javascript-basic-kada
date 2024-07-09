3f4d785fa77032a9359398642dff0a6a50c77a56
Shioya-yo <shioyay54@gmail.com>
2024-07-10 02:54:58 +0900

    Add files via upload

---
+++ kadai_005/bmi.js
@@ -0,0 +1,18 @@
+// 体重：68kg
+// 身長：1.7m
+// 計算式：[体重(kg)]÷[身長(m)×身長(m)]
+
+// 体重
+let bodyWeight;
+// 身長
+let height;
+
+bodyWeight=68;
+height=1.7;
+
+// 出力
+console.log(bodyWeight); 
+console.log(height); 
+
+// console.log(体重(kg)/身長(m)×身長(m));
+console.log(bodyWeight/height*height); 
